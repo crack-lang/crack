93d5751d3651abe56a68e3dbbe15e6991a171a6c
mmuller@enduden.com <mmuller@enduden.com>
2010-09-17 06:57:58 -0400

    Added a Poller class to crack.net, added tests to the main test suite.

--- runtime/Net.cc
+++ runtime/Net.cc
@@ -4,8 +4,11 @@
 #include <sys/socket.h>
 #include <poll.h>
 #include <arpa/inet.h>
+#include <string.h>
 #include <stdint.h>
+#include <malloc.h>
 #include <errno.h>
+#include <signal.h>
 
 #include <iostream>
 using namespace std;
@@ -32,7 +35,13 @@ struct _Constants {
         crk_SOCK_NONBLOCK,
         crk_SOCK_CLOEXEC,
         crk_SOL_SOCKET,
-        crk_SO_REUSEADDR;
+        crk_SO_REUSEADDR,
+        crk_POLLIN,
+        crk_POLLOUT,
+        crk_POLLPRI,
+        crk_POLLERR,
+        crk_POLLHUP,
+        crk_POLLNVAL;
     uint32_t crk_INADDR_ANY;
 };
 
@@ -58,6 +67,12 @@ static _Constants constants = {
     SOCK_CLOEXEC,
     SOL_SOCKET,
     SO_REUSEADDR,
+    POLLIN,
+    POLLOUT,
+    POLLPRI,
+    POLLERR,
+    POLLHUP,
+    POLLNVAL,
     INADDR_ANY
 };
 
@@ -70,6 +85,17 @@ struct SockAddrIn {
     struct crk_Object baseObject;
     uint32_t addr;
     unsigned int port;
+};
+
+struct TimeVal {
+    struct crk_Object baseObject;
+    int32_t secs, nsecs;
+};
+
+struct PollFD {
+    struct crk_Object baseObject;
+    int fd, events, revents;
+    void *pollable;
 };
 
 static void set_sockaddr_in(struct sockaddr_in *sa, SockAddrIn *addr) {
@@ -114,6 +140,101 @@ int _crack_accept(int s, SockAddrIn *addr) {
 
 int _crack_setsockopt_int(int fd, int level, int optname, int val) {
     return setsockopt(fd, level, optname, &val, sizeof(val));
+}
+
+pollfd *_crack_pollset_create(unsigned int size) {
+    return (pollfd *)calloc(size, sizeof(struct pollfd));
+}
+
+void _crack_pollset_copy(struct pollfd *dst, struct pollfd *src, 
+                         unsigned int size
+                         ) {
+    memcpy(dst, src, size * sizeof(struct pollfd));
+}
+
+void _crack_pollset_destroy(struct pollfd *pollset) {
+    free(pollset);
+}
+
+void _crack_pollset_set(struct pollfd *set, unsigned int index,
+                        int fd,
+                        int events,
+                        int revents
+                        ) {
+    struct pollfd &elem = set[index];
+    elem.fd = fd;
+    elem.events = events;
+    elem.revents = revents;
+}
+
+void _crack_pollset_get(struct pollfd *set, unsigned int index,
+                        PollFD *outputEntry
+                        ) {
+    struct pollfd &elem = set[index];
+    outputEntry->fd = elem.fd;
+    outputEntry->events = elem.fd;
+    outputEntry->revents = elem.fd;
+}
+
+// find the next poll entry that has an event in revents whose index is >= 
+// index.  Makes it easy to iterate over the pollset.  Returns the index of 
+// the item found, stores the entry info in outputEntry, -1 if no item was 
+// found.
+int _crack_pollset_next(struct pollfd *set, unsigned int size,
+                        unsigned int index,
+                        PollFD *outputEntry
+                        ) {
+    for (; index < size; ++index) {
+        pollfd *elem = &set[index];
+        if (elem->revents) {
+            outputEntry->fd = elem->fd;
+            outputEntry->events = elem->events;
+            outputEntry->revents = elem->revents;
+            return index;
+        }
+    }
+    
+    // not found.
+    return -1;
+}
+
+int _crack_poll(struct pollfd *fds, unsigned int nfds, TimeVal *tv,
+                sigset_t *sigmask
+                ) {
+    if (tv) {
+        struct timespec ts = {tv->secs, tv->nsecs};
+        return ppoll(fds, nfds, &ts, sigmask);
+    } else {
+        return ppoll(fds, nfds, 0, sigmask);
+    }
+}
+
+sigset_t *_crack_sigset_create() {
+    return (sigset_t *)malloc(sizeof(sigset_t));
+}
+
+void _crack_sigset_destroy(sigset_t *sigmask) {
+    free(sigmask);
+}
+
+int _crack_sigset_empty(sigset_t *sigmask) {
+    return sigemptyset(sigmask);
+}
+
+int _crack_sigset_fill(sigset_t *sigmask) {
+    return sigfillset(sigmask);
+}
+
+int _crack_sigset_add(sigset_t *sigmask, int signum) {
+    return sigaddset(sigmask, signum);
+}
+
+int _crack_sigset_del(sigset_t *sigmask, int signum) {
+    return sigdelset(sigmask, signum);
+}
+
+int _crack_sigset_has(sigset_t *sigmask, int signum) {
+    return sigismember(sigmask, signum);
 }
 
 } // extern "C"
