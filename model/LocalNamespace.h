0ce06c4bae2664c06a70684b22a528b54299bbf8
weyrick@mozek.us <weyrick@mozek.us>
2010-10-20 16:11:33 -0400

    handle case where parent is not empty, but name is

--- model/LocalNamespace.h
+++ model/LocalNamespace.h
@@ -16,8 +16,14 @@ class LocalNamespace : public Namespace {
 
     public:
         LocalNamespace(Namespace *parent, const std::string &cName) :
-                Namespace((parent && parent->getName().length())?
-                               parent->getName()+"."+cName:
+                // if we have a parent, create a canonical name
+                // based on it and the given cName. if cName is empty
+                // though (think local subcontext in a module), we use
+                // just the parent name. if we don't have a parent,
+                // we just use cName
+                Namespace((parent && !parent->getName().empty())?
+                               ((cName.empty())?parent->getName() :
+                                                parent->getName()+"."+cName) :
                                cName),
                 parent(parent) {}
         virtual NamespacePtr getParent(unsigned index);
