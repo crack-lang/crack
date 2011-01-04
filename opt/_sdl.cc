9539a6c1f5b4642a9b0c975b63b1bdc988cc12fa
mmuller@enduden.com <mmuller@enduden.com>
2011-01-04 06:34:56 -0500

    Support more event processing.

--- opt/_sdl.cc
+++ opt/_sdl.cc
@@ -1,6 +1,18 @@
 #include <SDL.h>
 SDL_Event *SDL_EventNew() { return new SDL_Event; }
 int SDL_Event_GetType(SDL_Event *evt) { return evt->type; }
+SDL_KeyboardEvent *SDL_Event_GetKey(SDL_Event *evt) { return &evt->key; }
+SDL_keysym *SDL_KeyboardEvent_GetKeysym(SDL_KeyboardEvent *evt) { return &evt->keysym; }
+unsigned SDL_keysym_GetScancode(SDL_keysym *s) { return s->scancode; }
+unsigned SDL_keysym_GetSym(SDL_keysym *s) { return s->sym; }
+unsigned SDL_keysym_GetMod(SDL_keysym *s) { return s->mod; }
+unsigned SDL_keysym_GetUnicode(SDL_keysym *s) { return s->unicode; }
+uint8_t SDL_KeyboardEvent_GetType(SDL_KeyboardEvent *evt) { return evt->type; }
+uint8_t SDL_KeyboardEvent_GetWhich(SDL_KeyboardEvent *evt) { return evt->which; }
+uint8_t SDL_KeyboardEvent_GetState(SDL_KeyboardEvent *evt) { return evt->state; }
+SDL_MouseMotionEvent *SDL_Event_GetMotion(SDL_Event *evt) { return &evt->motion; }
+uint SDL_MouseMotionEvent_GetX(SDL_MouseMotionEvent *evt) { return evt->x; }
+uint SDL_MouseMotionEvent_GetY(SDL_MouseMotionEvent *evt) { return evt->y; }
 
 
 #include "ext/Module.h"
@@ -15,8 +27,17 @@ void crack_ext__sdl_init(crack::ext::Module *mod) {
     crack::ext::Type *type_SDL_Event = mod->addType("SDL_Event");
     type_SDL_Event->finish();
 
+    crack::ext::Type *type_SDL_KeyboardEvent = mod->addType("SDL_KeyboardEvent");
+    type_SDL_KeyboardEvent->finish();
+
+    crack::ext::Type *type_SDL_MouseMotionEvent = mod->addType("SDL_MouseMotionEvent");
+    type_SDL_MouseMotionEvent->finish();
+
     crack::ext::Type *type_SDL_Surface = mod->addType("SDL_Surface");
     type_SDL_Surface->finish();
+
+    crack::ext::Type *type_SDL_keysym = mod->addType("SDL_keysym");
+    type_SDL_keysym->finish();
 
     crack::ext::Type *array = mod->getType("array");
     crack::ext::Type *type_int = mod->getIntType();
@@ -66,6 +87,72 @@ void crack_ext__sdl_init(crack::ext::Module *mod) {
                      (void *)SDL_Event_GetType
                      );
     f->addArg(type_SDL_Event, "event");
+
+    f = mod->addFunc(type_SDL_KeyboardEvent, "SDL_Event_GetKey",
+                     (void *)SDL_Event_GetKey
+                     );
+    f->addArg(type_SDL_Event, "evt");
+
+    f = mod->addFunc(type_SDL_keysym, "SDL_KeyboardEvent_GetKeysym",
+                     (void *)SDL_KeyboardEvent_GetKeysym
+                     );
+    f->addArg(type_SDL_KeyboardEvent, "evt");
+
+    f = mod->addFunc(type_uint, "SDL_keysym_GetScancode",
+                     (void *)SDL_keysym_GetScancode
+                     );
+    f->addArg(type_SDL_keysym, "keysym");
+
+    f = mod->addFunc(type_uint, "SDL_keysym_GetSym",
+                     (void *)SDL_keysym_GetSym
+                     );
+    f->addArg(type_SDL_keysym, "keysym");
+
+    f = mod->addFunc(type_uint, "SDL_keysym_GetMod",
+                     (void *)SDL_keysym_GetMod
+                     );
+    f->addArg(type_SDL_keysym, "keysym");
+
+    f = mod->addFunc(type_uint, "SDL_keysym_GetUnicode",
+                     (void *)SDL_keysym_GetUnicode
+                     );
+    f->addArg(type_SDL_keysym, "keysym");
+
+    f = mod->addFunc(type_byte, "SDL_KeyboardEvent_GetType",
+                     (void *)SDL_KeyboardEvent_GetType
+                     );
+    f->addArg(type_SDL_KeyboardEvent, "evt");
+
+    f = mod->addFunc(type_byte, "SDL_KeyboardEvent_GetWhich",
+                     (void *)SDL_KeyboardEvent_GetWhich
+                     );
+    f->addArg(type_SDL_KeyboardEvent, "evt");
+
+    f = mod->addFunc(type_byte, "SDL_KeyboardEvent_GetState",
+                     (void *)SDL_KeyboardEvent_GetState
+                     );
+    f->addArg(type_SDL_KeyboardEvent, "evt");
+
+    f = mod->addFunc(type_SDL_MouseMotionEvent, "SDL_Event_GetMotion",
+                     (void *)SDL_Event_GetMotion
+                     );
+    f->addArg(type_SDL_Event, "evt");
+
+    f = mod->addFunc(type_uint, "SDL_MouseMotionEvent_GetX",
+                     (void *)SDL_MouseMotionEvent_GetX
+                     );
+    f->addArg(type_SDL_MouseMotionEvent, "evt");
+
+    f = mod->addFunc(type_uint, "SDL_MouseMotionEvent_GetY",
+                     (void *)SDL_MouseMotionEvent_GetY
+                     );
+    f->addArg(type_SDL_MouseMotionEvent, "evt");
+
+    f = mod->addFunc(type_void, "SDL_WarpMouse",
+                     (void *)SDL_WarpMouse
+                     );
+    f->addArg(type_uint, "x");
+    f->addArg(type_uint, "y");
 
     f = mod->addFunc(type_int, "SDL_PollEvent",
                      (void *)SDL_PollEvent
@@ -164,6 +251,10 @@ void crack_ext__sdl_init(crack::ext::Module *mod) {
                      static_cast<int>(SDL_NOFRAME)
                      );
 
+    mod->addConstant(type_uint32, "SDL_NOEVENT",
+                     static_cast<int>(SDL_NOEVENT)
+                     );
+
     mod->addConstant(type_uint32, "SDL_KEYDOWN",
                      static_cast<int>(SDL_KEYDOWN)
                      );
@@ -174,6 +265,58 @@ void crack_ext__sdl_init(crack::ext::Module *mod) {
 
     mod->addConstant(type_uint32, "SDL_MOUSEMOTION",
                      static_cast<int>(SDL_MOUSEMOTION)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_MOUSEBUTTONDOWN",
+                     static_cast<int>(SDL_MOUSEBUTTONDOWN)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_MOUSEBUTTONUP",
+                     static_cast<int>(SDL_MOUSEBUTTONUP)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_JOYAXISMOTION",
+                     static_cast<int>(SDL_JOYAXISMOTION)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_JOYBALLMOTION",
+                     static_cast<int>(SDL_JOYBALLMOTION)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_JOYHATMOTION",
+                     static_cast<int>(SDL_JOYHATMOTION)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_JOYBUTTONDOWN",
+                     static_cast<int>(SDL_JOYBUTTONDOWN)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_JOYBUTTONUP",
+                     static_cast<int>(SDL_JOYBUTTONUP)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_QUIT",
+                     static_cast<int>(SDL_QUIT)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_SYSWMEVENT",
+                     static_cast<int>(SDL_SYSWMEVENT)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_EVENT_RESERVED2",
+                     static_cast<int>(SDL_EVENT_RESERVED2)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_EVENT_RESERVED3",
+                     static_cast<int>(SDL_EVENT_RESERVED3)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_USEREVENT",
+                     static_cast<int>(SDL_USEREVENT)
+                     );
+
+    mod->addConstant(type_uint32, "SDL_NUMEVENTS",
+                     static_cast<int>(SDL_NUMEVENTS)
                      );
 
     mod->addConstant(type_uint32, "SDL_GL_RED_SIZE",
