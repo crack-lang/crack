8c02ed9d4b2d39eff39cdc7ab367353bc1913098
Arno Rehn <arno@arnorehn.de>
2012-07-29 22:45:21 +0200

    make overrides implicitly virtual.

--- ext/Func.cc
+++ ext/Func.cc
@@ -124,6 +124,11 @@ void Func::finish() {
 
         VarDefPtr existingDef = parser.checkForExistingDef(nameTok, name, true);
         override = parser.checkForOverride(existingDef.get(), realArgs, context->ns.get(), nameTok, name);
+
+        // make overrides implicitly virtual
+        if (override) {
+            flags = static_cast<Func::Flags>(flags | virtualized);
+        }
     }
 
     // if we have a function pointer, create a extern function for it
@@ -137,6 +142,10 @@ void Func::finish() {
         } else if (override) {
             // if this is an override, create a wrapper function
             externName = name + ":impl_" + receiverType->name;
+
+            // this is only the extern function, it can't be virtual.
+            // the wrapper for it, further down, is.
+            flags = static_cast<Func::Flags>(flags & ~virtualized);
         } else {
             externName = name;
         }
