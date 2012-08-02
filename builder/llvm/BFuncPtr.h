bf287099d68a47428399f52f042d32867993b701
mmuller@google.com <mmuller@google.com>
2012-08-02 08:11:26 -0400

    Manually fixed up some of the copyrights, fixed unit tests, recorded more information in the README.

--- builder/llvm/BFuncPtr.h
+++ builder/llvm/BFuncPtr.h
@@ -1,9 +1,10 @@
 // Copyright 2011-2012 Shannon Weyrick <weyrick@mozek.us>
-// 
+// Copyright 2012 Google Inc.
+//
 //   This Source Code Form is subject to the terms of the Mozilla Public
 //   License, v. 2.0. If a copy of the MPL was not distributed with this
 //   file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// 
+//
 
 #ifndef _builder_llvm_BFuncPtr_h_
 #define _builder_llvm_BFuncPtr_h_
--- builder/llvm/BResultExpr.h
+++ builder/llvm/BResultExpr.h
@@ -1,4 +1,5 @@
 // Copyright 2010 Shannon Weyrick <weyrick@mozek.us>
+// Copyright 2011 Google Inc.
 // 
 //   This Source Code Form is subject to the terms of the Mozilla Public
 //   License, v. 2.0. If a copy of the MPL was not distributed with this
