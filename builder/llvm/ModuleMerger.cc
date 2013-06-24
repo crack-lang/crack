9884fc167f7c63322c89dbeb1d1221fcb6210a8a
mmuller@google.com <mmuller@google.com>
2013-06-24 07:57:23 -0400

    Added ModuleMerger, the first pass of a stripped-down LLVM Linker replacement that preserves types.

---
+++ builder/llvm/ModuleMerger.cc
@@ -0,0 +1,148 @@
+// Copyright 2013 Google Inc.
+//
+//   This Source Code Form is subject to the terms of the Mozilla Public
+//   License, v. 2.0. If a copy of the MPL was not distributed with this
+//   file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+#include "ModuleMerger.h"
+
+#include <iostream>
+
+#include <llvm/DerivedTypes.h>
+#include <llvm/Function.h>
+#include <llvm/GlobalValue.h>
+#include <llvm/Instructions.h>
+#include <llvm/LLVMContext.h>
+#include <llvm/Module.h>
+#include <llvm/Transforms/Utils/Cloning.h>
+
+using namespace builder::mvll;
+using namespace llvm;
+using namespace std;
+
+bool ModuleMerger::defined(GlobalValue *gval) {
+    if (gval->hasName() && !gval->hasLocalLinkage()) {
+        GlobalValue *targetGVal = target->getNamedValue(gval->getName());
+
+        // if it's already defined in the target, make sure it's in the value
+        // map, too, then fall through to returning "true".
+        if (targetGVal)
+            valueMap[gval] = targetGVal;
+        else
+            return false;
+    }
+
+    return true;
+}
+
+void ModuleMerger::copyGlobalAttrs(GlobalValue *dst, GlobalValue *src) {
+    dst->copyAttributesFrom(src);
+    dst->setAlignment(src->getAlignment());
+}
+
+void ModuleMerger::addGlobalDeclaration(GlobalVariable *gvar) {
+    if (defined(gvar))
+        return;
+
+    GlobalVariable *newGVar =
+        new GlobalVariable(*target, gvar->getType()->getElementType(),
+                           gvar->isConstant(),
+                           gvar->getLinkage(),
+                           0, // init
+                           gvar->getName(),
+                           0, // insert before
+                           gvar->getThreadLocalMode(),
+                           gvar->getType()->getAddressSpace()
+                           );
+
+    copyGlobalAttrs(newGVar, gvar);
+    valueMap[gvar] = newGVar;
+}
+
+void ModuleMerger::addFunctionDeclaration(Function *func) {
+    if (defined(func))
+        return;
+
+    cerr << "adding function declaration for " << func->getName().str() <<
+        " @" << func << endl;
+    Function *newFunc = Function::Create(func->getFunctionType(),
+                                         func->getLinkage(),
+                                         func->getName(),
+                                         target
+                                         );
+    copyGlobalAttrs(newFunc, func);
+    valueMap[func] = newFunc;
+}
+
+void ModuleMerger::addInitializer(GlobalVariable *gvar) {
+    if (defined(gvar))
+        return;
+
+    GlobalVariable *dest = cast<GlobalVariable>(valueMap[gvar]);
+    dest->setInitializer(MapValue(gvar->getInitializer(), valueMap,
+                                  RF_None
+                                  )
+                         );
+}
+
+void ModuleMerger::addFunctionBody(Function *func) {
+    string errorMsg;
+
+    cerr << "adding function body for " << func->getName().str() <<
+        " @" << func << endl;
+
+    if (func->isDeclaration()) {
+        if (!func->isMaterializable())
+            return;
+        else
+            func->Materialize(&errorMsg);
+    }
+
+    Function *dest = cast<Function>(valueMap[func]);
+
+    // We should already have an argument list, copy the names and add the
+    // arguments to the value map.
+    for (Function::arg_iterator arg = func->arg_begin(),
+          destArg = dest->arg_begin();
+         arg != func->arg_end();
+         ++arg
+         ) {
+        destArg->setName(arg->getName());
+        valueMap[arg] = destArg;
+    }
+
+    SmallVector<ReturnInst *, 8> returns;
+    CloneFunctionInto(dest, func, valueMap, false, returns, "");
+}
+
+ModuleMerger::ModuleMerger(const string &name) {
+    LLVMContext &lctx = getGlobalContext();
+    target = new llvm::Module(name, lctx);
+}
+
+void ModuleMerger::merge(Module *module) {
+    cerr << ">>> running merge on " << module->getModuleIdentifier() << endl;
+    for (Module::global_iterator i = module->global_begin();
+         i != module->global_end();
+         ++i
+         )
+        addGlobalDeclaration(i);
+
+    for (Module::iterator i = module->begin(); i != module->end(); ++i)
+        addFunctionDeclaration(i);
+
+    // update the global initializers.
+    for (Module::global_iterator i = module->global_begin();
+         i != module->global_end();
+         ++i
+         ) {
+        if (i->hasInitializer())
+            addInitializer(i);
+    }
+
+    // copy the function bodies.
+    for (Module::iterator i = module->begin(); i != module->end(); ++i)
+        addFunctionBody(i);
+    cerr << ">>> done with merge on " << module->getModuleIdentifier() << endl;
+}
