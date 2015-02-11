e9a25f0ba2e0137371011dad343b680c45c3398f
Michael Muller <mmuller@google.com>
2015-02-11 06:46:17 -0500

    Check for undefined forward type declarations at the end of the block.

--- model/Context.cc
+++ model/Context.cc
@@ -361,7 +361,7 @@ void Context::checkForUnresolvedForwards() {
         
         // Check for an unresolved type.
         if (TypeDef *type = TypeDefPtr::rcast(iter->second)) {
-            if (type->forward)
+            if (type->forward && type->getOwner() == ns)
                 error(SPUG_FSTR("Forward declared type not defined at the end " 
                                  "of the block: " << type->getDisplayName()
                                 )
