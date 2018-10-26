77349007bbe62695231f234769dd84a72b8dbe06
Michael Muller <mmuller@google.com>
2018-10-26 17:04:35 -0400

    Updated shell script to build under docker.

--- docker/build-crack.sh
+++ docker/build-crack.sh
@@ -5,22 +5,22 @@ set -e
 cd /
 apt-get update
 apt-get upgrade -y
-apt-get install -y mercurial wget autoconf automake libtool gettext build-essential libpcre3-dev
-hg clone https://crack-language.googlecode.com/hg/ crack
+apt-get install -y git wget autoconf automake libtool gettext build-essential libpcre3-dev python
+git clone https://github.com/crack-lang/crack
 wget http://llvm.org/releases/3.3/llvm-3.3.src.tar.gz
 tar -xvzf llvm-3.3.src.tar.gz
 
 # Build LLVM.
 cd /llvm-3.3.src
 ./configure
-make REQUIRES_RTTI=1
+make -j5 REQUIRES_RTTI=1
 make install
 
 # Build crack from head.
 cd /crack
 ./bootstrap
 ./configure
-make
+make -j5
 make install
 ldconfig
 
